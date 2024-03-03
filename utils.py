7e5351f8e3e99775690ce9208977caaa78c5a934
Sahil Shah <sahilshah2104@gmail.com>
2024-03-03 20:08:04 +0530

    Utilities for Root File

---
+++ utils.py
@@ -0,0 +1,43 @@
+import os
+import torch
+import torchvision
+from PIL import Image
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+
+
+def plot_images(images):
+    plt.figure(figsize=(32, 32))
+    plt.imshow(torch.cat([
+        torch.cat([i for i in images.cpu()], dim=-1),
+    ], dim=-2).permute(1, 2, 0).cpu())
+    plt.show()
+
+
+def save_images(images, path, **kwargs):
+    grid = torchvision.utils.make_grid(images, **kwargs)
+    ndarr = grid.permute(1, 2, 0).to('cpu').numpy()
+    im = Image.fromarray(ndarr)
+    im.save(path)
+
+
+def get_data(args):
+    transform = transforms.Compose([
+        transforms.Resize((args.image_size, args.image_size)),
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = ImageFolder(root=os.path.join(args.dataset_path, "train"), transform=transform)
+    test_dataset = ImageFolder(root=os.path.join(args.dataset_path, "test"), transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
+
+def setup_logging(run_name):
+    os.makedirs("models", exist_ok=True)
+    os.makedirs("results", exist_ok=True)
+    os.makedirs(os.path.join("models", run_name), exist_ok=True)
+    os.makedirs(os.path.join("results", run_name), exist_ok=True)
